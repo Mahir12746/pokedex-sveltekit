2e2bb3e02dbc28c40595ad7b83d1d414b4689b55
Mahir12746 <mahiraseef@gmail.com>
2024-07-17 21:05:34 +0600

    fetch and display data from pokeapi.co

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,86 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+    import type {PageData} from "./$types";
+    import {generations} from "./generations";
+
+    export let data: PageData;
+</script>
+
+
+<div class="generations">
+    {#each generations as generation (generation.id)}
+        <div class="generation">{generation.main_region}</div>
+    {/each}
+</div>
+
+
+<div class="monsters">
+    {#each data.monsters as monster (monster.id)}
+        <div class="monster">
+            <div class="monster-content">
+                <img src="{monster.image}" alt={monster.name}>
+                {monster.name}
+            </div>
+            <div class="monster-id">
+                {monster.id}
+            </div>
+
+        </div>
+    {/each}
+</div>
+
+
+<style>
+
+    .generations {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .generation {
+        margin: 5px;
+        padding: 5px 10px;
+        border: 2px solid #aaa;
+        border-radius: 6px;
+        background-color: #f9f9f9;
+        color: #333;
+    }
+
+    .generation:hover {
+        background-color: #f0f0f0;
+    }
+
+    .monsters {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .monster-id {
+        position: absolute;
+        top: 8px;
+        left: 8px;
+        font-size: 0.8em;
+        color: #aaa;
+    }
+
+    .monster {
+        width: 100px;
+        margin: 10px;
+        padding: 10px;
+        position: relative;
+        background-color: #eee;
+    }
+
+    .monster:hover {
+        background-color: #ddd;
+    }
+
+    .monster-content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
